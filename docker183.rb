4c6b2818154037dff9a5546cb077390a63114c93
Red54 <Yeking@Red54.com>
2015-12-10 21:07:49 +0800

    docker183 1.8.3 (new formula)
    Signed-off-by: 謝致邦 <Yeking@Red54.com>

---
+++ docker183.rb
@@ -0,0 +1,31 @@
+class Docker183 < Formula
+  desc "Pack, ship and run any application as a lightweight container"
+  homepage "https://www.docker.com/"
+  url "https://github.com/docker/docker.git", :tag => "v1.8.3",
+                                              :revision => "f4bf5c7026816785d9f63c07e87f9450a49f2403"
+
+  option "with-experimental", "Enable experimental features"
+  option "without-completions", "Disable bash/zsh completions"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["AUTO_GOPATH"] = "1"
+    ENV["DOCKER_CLIENTONLY"] = "1"
+    ENV["DOCKER_EXPERIMENTAL"] = "1" if build.with? "experimental"
+
+    system "hack/make.sh", "dynbinary"
+
+    bin.install "bundles/#{version}/dynbinary/docker-#{version}" => "docker"
+
+    if build.with? "completions"
+      bash_completion.install "contrib/completion/bash/docker"
+      fish_completion.install "contrib/completion/fish/docker.fish"
+      zsh_completion.install "contrib/completion/zsh/_docker"
+    end
+  end
+
+  test do
+    system "#{bin}/docker", "--version"
+  end
+end
